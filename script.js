ac62639759380c7eb37a02dd45e63d1a39549faa
FileOtter <kevin.t.connell@gmail.com>
2020-09-07 14:03:36 -0400

    Added a progress bar, made a timer function, added event listener for anser choices, and made it all display on the screen correctly

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 var masterList = [
-    {   query: "What is your name?",
+    {
+        query: "What is your name?",
         right: "Kevin",
         wrong1: "Jim",
         wrong2: "Bob",
         wrong3: "Carol",
     },
-    {   query: "What is your age?",
+    {
+        query: "What is your age?",
         right: "24",
         wrong1: "23",
         wrong2: "22",
         wrong3: "21",
     },
-    {   query: "Where do you live?",
+    {
+        query: "Where do you live?",
         right: "West Philly",
         wrong1: "Nebraska",
         wrong2: "South Philly",
         wrong3: "New York",
     },
-    {   query: "Are you stunningly gorgeous?",
+    {
+        query: "Are you stunningly gorgeous?",
         right: "Yes",
         wrong1: "Maybe",
         wrong2: "Sometimes",
         wrong3: "No",
     },
-    {   query: "What is your favorite color?",
+    {
+        query: "What is your favorite color?",
         right: "Green",
         wrong1: "Red",
         wrong2: "Black",
@@ -31,24 +36,74 @@ var masterList = [
     }
 ]
 
-console.log(masterList[2].query);
-
-
-// for (var i = 0; i < masterKeys.length; i++) {
-//     alert(masterList.(masterKeys[i]).query)
-//     randomizeOptions(masterList.(masterKeys[1])
-// }
-
 var timeLeft = 60
+var timerEl = document.querySelector("#timer");
 var answerOptions = ["1", "2", "3", "4"]
-
 var answer1 = document.querySelector("#a1");
 var answer2 = document.querySelector("#a2");
 var answer3 = document.querySelector("#a3");
 var answer4 = document.querySelector("#a4");
 var question = document.querySelector("#question");
+var progressEl = document.querySelector("#progress")
+var tileEl = document.querySelector("#quizTile")
+var statusEl = document.querySelector("#status")
+var progressPerc = "0%"
+// for (var i = 0; i < masterList.length - 3; i++) {
+//     var progressPerc = (i/masterList.length) * 100 + "%"
+//     newQuestion(masterList[i])
+
+// }
+tileEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (event.target.textContent == "Start") {
+        console.log("it matches Start");
+        setTime();
+        statusEl.textContent = "...";
+        i = 0;
+
+        newQuestion(masterList[i])
+    }
+    if (event.target.matches(".answer")) {
+        console.log("You clicked an answer!")
+        if (event.target.textContent == masterList[i].right) {
+            statusEl.textContent = "Correct!";
+            i++;
+            newQuestion(masterList[i])
+            setTimeout(function () { statusEl.textContent = "..."; }, 4000);
+        } else {
+            statusEl.textContent = "Wrong!";
+            timerEl.setAttribute("style", "color : red");
+            if (timeLeft > 10) {
+                timeLeft -= 10
+            } else {
+                timeLeft = 0
+            }
+            i++;
+            newQuestion(masterList[i])
+            setTimeout(function () { statusEl.textContent = "..."; }, 2000);
+            setTimeout(function () { timerEl.setAttribute("style", "color : black"); }, 2000);
+        }
+    }
+
+
+});
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = timeLeft
+
+        if (timeLeft === 0 || i == masterList.length) {
+            clearInterval(timerInterval);
+            alert("Your score is " + timeLeft)
+        }
+
+    }, 1000);
+}
 
 function newQuestion(x) {
+    progressPerc = (i / masterList.length) * 100 + "%"
+    progressEl.setAttribute("style", "width : " + progressPerc);
     question.textContent = x.query;
     answerOptions = [x.right, x.wrong1, x.wrong2, x.wrong3];
     console.log(answerOptions)
@@ -59,6 +114,3 @@ function newQuestion(x) {
     answer3.textContent = answerOptions[2];
     answer4.textContent = answerOptions[3];
 }
-console.log(answer1)
-
-newQuestion(masterList[2])
